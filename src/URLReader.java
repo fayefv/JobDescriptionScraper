7d72f2f66de60b3a90455d02ba8b17dd7afcb549
Faye Fong <fong.faye@gmail.com>
2022-09-20 22:54:53 -0700

    tried using headers to slice flattened text but this was messy and led to loss of implied grammar order
    currently using specific CSS selectors to get custom output

--- src/URLReader.java
+++ src/URLReader.java
@@ -1,28 +1,19 @@
 /*
 Takes job descriptions in the form of URLs and possibly files and stores raw text.
 Raw text can be parsed in a number of ways.
- */
-//public class JobLoader {
-//
-//    public JobLoader(String url) {
-//        // TODO: choose some methodology for cURLing
-//    }
-//}
+*/
 
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
-import java.sql.Array;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.*;
+import java.util.HashMap;
 
 
 public class URLReader {
     public static void main(String[] args) {
 
-        String title = "";
         Document doc = null;
         try {
             doc = Jsoup.connect("https://gilead.yello.co/jobs/SFVW6FkP2XDe_-o0rfhM2Q?job_board_id=v42vD4vKxb3AkKvV93YsrQ").get();
@@ -30,13 +21,49 @@ public class URLReader {
             e.printStackTrace();
         }
 
-        var headers = doc.select("h1, h2, h3, h4, h5, h6");
-        var delimiters = new ArrayList<String>();
-        for (var h : headers) { // Headers used to separate sections in the document
-            if (h.hasText())
-                delimiters.add(h.text());
-        }
 
+
+        // System.out.println(doc.getAllElements());
+
+        System.out.println(doc.select("h1, h1 ~ p, h1 ~ ul, h3, h3 ~ p, h3 ~ul"));
+
+
+
+
+
+
+
+
+
+//        var content = doc.text();
+//        var headers = doc.select("h1, h2, h3, h4, h5, h6");
+//        var slicedContent = new HashMap<String, String>();
+//
+//
+//        var sectionTitles = new ArrayList<String>();
+//        for (var h : headers) {
+//            if (h.hasText()) sectionTitles.add(h.text());
+//        }
+
+//        for (var x : sectionTitles)
+//            System.out.println((x));
+
+//       var content0 = StringUtils.substringBetween(content, sectionTitles.get(0), sectionTitles.get(1));
+//
+//
+//        System.out.println(content0);
+
+
+//        for (int idx = 0; idx < sectionTitles.size() - 1; idx++) {
+//            slicedContent.put(sectionTitles.get(idx), StringUtils.substringBetween(content, sectionTitles.get(idx), sectionTitles.get(idx + 1)));
+//
+//        }
+//
+//
+//        for( var k : slicedContent.keySet()){
+//            System.out.println(k + ", " + slicedContent.get(k));
+//
+//        }
 
     }
 }
